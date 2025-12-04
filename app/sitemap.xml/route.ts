c4f79431db20c357c26669c63866794fe3ff918c
Grakri123 <kgranli-99@live.no>
2025-12-04 14:13:38 +0100

    lagt til rikti sitemap

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,25 @@
+export async function GET() {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://ailabben.no'
+  const currentDate = new Date().toISOString()
+
+  // Generate sitemap index XML
+  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${baseUrl}/sitemap-pages.xml</loc>
+    <lastmod>${currentDate}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>${baseUrl}/sitemap-blog.xml</loc>
+    <lastmod>${currentDate}</lastmod>
+  </sitemap>
+</sitemapindex>`
+
+  return new Response(sitemapIndex, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+    },
+  })
+}
+
